1df5cdb76130416c4ab5dd9ae2de3f0529532541
unknown <margarita.salazar.mls@gmail.com>
2021-04-09 12:36:23 -0500

    made test for form header renders and form shows success message on submit with form details

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,45 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+    const header = screen.getByText(/Checkout Form/i)
+
+    expect(header).toBeInTheDocument()
+});
+
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm />)
+
+    const firstName = screen.getByLabelText(/First Name:/i)
+    userEvent.type(firstName, "John")
+
+    const lastName = screen.getByLabelText(/Last Name:/i)
+    userEvent.type(lastName, "Doe")
+
+    const address = screen.getByLabelText(/Address:/i)
+    userEvent.type(address, "1234 Main Street")
+
+    const city = screen.getByLabelText(/City:/i)
+    userEvent.type(city, "City")
+
+    const state = screen.getByLabelText(/State:/i)
+    userEvent.type(state, "State")
+
+    const zip = screen.getByLabelText(/Zip:/i)
+    userEvent.type(zip, "1234")
+
+    const button = screen.getByRole("button")
+    userEvent.click(button)
+
+    
+    const message = await screen.getByText("You have ordered some plants! Woo-hoo!")
+    expect(message).toBeInTheDocument();
+
+    
+});
